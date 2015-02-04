33544a91eded18c088780f6a57f07382a5194d9c
dfucci <davfuc@gmail.com>
2015-02-04 09:23:10 +0200

    add answer to question feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :answers
+
   devise_for :users
   root "questions#index"
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
