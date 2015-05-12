409a6fbab60d6f694c742af3991f10f42cff92b1
dfucci <davfuc@gmail.com>
2015-05-11 20:32:44 -0700

    fixed role init for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :admin]
+  enum role: [:user, :admin, :attorney, :student]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role || :user
+    self.role ||= :user
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
