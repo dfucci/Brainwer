f90e135ebc7bfcebd91feb3b9a905fad7eebcfab
dfucci <davfuc@gmail.com>
2015-05-20 20:30:09 -0700

    translated into italian

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521021705) do
+ActiveRecord::Schema.define(version: 20150521022351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150521021705) do
     t.boolean  "admin",                              default: false
     t.integer  "role"
     t.string   "name"
+    t.string   "surname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
