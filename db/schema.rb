df8fca11d6f120fa87f0c8551d0105984667320c
dfucci <davfuc@gmail.com>
2015-05-21 18:37:10 -0700

    new settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521022351) do
+ActiveRecord::Schema.define(version: 20150522010126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,13 @@ ActiveRecord::Schema.define(version: 20150521022351) do
     t.integer  "role"
     t.string   "name"
     t.string   "surname"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
