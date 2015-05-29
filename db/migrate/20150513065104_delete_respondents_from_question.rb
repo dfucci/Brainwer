66b43166722fc4b7c0ec67a891addd6551892f1f
dfucci <davfuc@gmail.com>
2015-05-28 18:54:32 -0700

    removed unseful migratuons

--- db/migrate/20150513065104_delete_respondents_from_question.rb
+++
@@ -1,5 +0,0 @@
-class DeleteRespondentsFromQuestion < ActiveRecord::Migration
-  def change
-    remove_column :questions, :respondents
-  end
-end
--- db/migrate/20150513065455_add_respndents_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddRespndentsToQuestions < ActiveRecord::Migration
-  def change
-    add_column :questions, :respondents, :string, array: true, default: '{}'
-  end
-end
