8498b9abe10f032d5ffa71039e3a571ece151c65
dfucci <davfuc@gmail.com>
2015-05-28 18:18:54 -0700

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :exception
+  before_filter :configure_devise_parameters, if: :devise_controller?
+
+  protected
+  def configure_devise_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :surname
+    devise_parameter_sanitizer.for(:account_update) << :surname
+  end
 end
